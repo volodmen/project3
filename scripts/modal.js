d7ca7c09b5f3bedccdaabe749f10d19b4bfc8822
volodmen <130523382+volodmen@users.noreply.github.com>
2023-07-06 21:47:05 +1000

    Add files via upload

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,19 +1,34 @@
-$('.form').submit((e) =>{
+const validateFields = (form, fieldsArray) => {
+    fieldsArray.forEach((field) => {
+      field.removeClass("input-error");
+      if (field.val().trim() === "") {
+        field.addClass("input-error");
+      }
+    });
+  
+    const errorFields = form.find(".input-error");
+  
+    return errorFields.length === 0;
+  };
+  
+  $(".form").submit((e) => {
     e.preventDefault();
-
+  
     const form = $(e.currentTarget);
     const name = form.find("[name='name']");
     const phone = form.find("[name='phone']");
     const comment = form.find("[name='comment']");
     const to = form.find("[name='to']");
-
-    [name, phone, comment, to].forEach(field => {
-        if (field.val()=== ""){
-            field.addClass("input-error");
-        }
-    });
-
-    $.ajax({
+  
+    const modal = $("#modal");
+    const content = modal.find(".modal__content");
+  
+    modal.removeClass("error-modal");
+  
+    const isValid = validateFields(form, [name, phone, comment, to]);
+  
+    if (isValid) {
+      const request = $.ajax({
         url: "https://webdev-api.loftschool.com/sendmail",
         method: "post",
         data: {
@@ -24,15 +39,30 @@ $('.form').submit((e) =>{
         },
         error: (data) => {},
       });
-
-    $.fancybox.open({
-        src: "#modal",
-        type: "inline"
-    })
-}); 
-
-$(".app-submit-btn").click(e => {
+  
+      request.done((data) => {
+        content.text(data.message);
+      });
+  
+      request.always(() => {
+        $.fancybox.open({
+          src: "#modal",
+          type: "inline",
+        });
+      });
+    }
+  });
+  
+  $(".app-submit-btn").click((e) => {
     e.preventDefault();
-
+    const form = $('form');
+    const name = form.find("[name='name']");
+    const phone = form.find("[name='phone']");
+    const comment = form.find("[name='comment']")
+  
+    comment.val('');
+    name.val("")
+    phone.val("")
+    
     $.fancybox.close();
-});
+  });
